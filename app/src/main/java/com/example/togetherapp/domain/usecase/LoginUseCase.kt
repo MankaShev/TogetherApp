ac93f0ec199537d8c2d62d353acc5fbfe5c15f9c
Yevstremskaya_Ekaterina <kataevstremskaa@gmail.com>
2026-03-10 01:21:36 +0700

    add domain layer: models, repository interface, usecases

---
+++ app/src/main/java/com/example/togetherapp/domain/usecase/LoginUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.togetherapp.domain.usecase
+
+class LoginUseCase {
+    fun execute(email: String, password: String): Boolean {
+        // так как прототип=  вернуть true
+        return true
+    }
+}
