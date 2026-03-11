9cb477643de145ffc3b750bb045f6ce3d115ad34
thetallproblem <101548844+thetallproblem@users.noreply.github.com>
2026-03-11 23:51:51 +0700

    Навигация

--- app/src/main/java/com/example/togetherapp/presentation/screens/home/HomeActivity.kt
+++ app/src/main/java/com/example/togetherapp/presentation/screens/home/HomeActivity.kt
@@ -1,16 +1,31 @@
 package com.example.togetherapp.presentation.screens.home
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import androidx.activity.ComponentActivity
 import com.example.togetherapp.R
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.togetherapp.presentation.screens.map.MapActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : ComponentActivity() {
+
+    private lateinit var mapButton: Button
+    private lateinit var createButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.home_screen)
+
+        mapButton = findViewById(R.id.go_to_map_button)
+
+        mapButton.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
