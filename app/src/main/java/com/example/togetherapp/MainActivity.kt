6e632cb5878969a1e8437e87a6b4652204284f41
thetallproblem <101548844+thetallproblem@users.noreply.github.com>
2026-03-11 08:37:34 +0700

    Добавление экранов

--- app/src/main/java/com/example/togetherapp/MainActivity.kt
+++ app/src/main/java/com/example/togetherapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.togetherapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +13,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.togetherapp.presentation.screens.home.HomeActivity
 import com.example.togetherapp.ui.theme.TogetherAppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var login_Activity_button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,6 +31,13 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+        setContentView(R.layout.login_screen)
+
+        login_Activity_button = findViewById(R.id.login_button)
+
+        login_Activity_button.setOnClickListener { 
+            val intent = Intent(this, HomeActivity::class.java)
         }
     }
 }
